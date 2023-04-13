d07885d4ad47ee868d6d97f9ce03e54960952807
OPTIMISSA\oscar.pastor <oscar.pastor@optimissa.com>
2023-04-13 11:29:45 +0200

    Se implementa suma y resta

---
+++ DemoGit/src/Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora {
+    public static void main (String [ ] args) {
+
+        Suma.suma(4,+3);
+        Resta.resta(5,3);
+
+    }
+}
---
+++ DemoGit/src/Suma.java
@@ -0,0 +1,5 @@
+public class Suma {
+    public static int suma(int op1, int op2) {
+        return op1 + op2;
+    }
+}
